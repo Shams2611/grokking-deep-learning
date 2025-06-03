a828955dc2cf67ab3dd6cddca16da02c1c214d63
Shams Abrar <shams.abrar2611@gmail.com>
2025-06-03 10:00:00 -0400

    Chapter 3: 09_dot_product_intro

---
+++ Chapter 3/09_dot_product_intro.py
@@ -0,0 +1,15 @@
+# DOT PRODUCT
+# multiply corresponding elements, then sum
+
+# a . b = a1*b1 + a2*b2 + a3*b3
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+# manual dot product
+dot = a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"a . b = {dot}")
+
+# same as weighted sum!
