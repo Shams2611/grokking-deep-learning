a07a7368f1d65e252a9aa9f34002dc8129bfed31
Shams Abrar <shams.abrar2611@gmail.com>
2025-06-22 10:00:00 -0400

    Chapter 4: 08_gradient_intuition

---
+++ Chapter 4/08_gradient_intuition.py
@@ -0,0 +1,17 @@
+# gradient intuition
+
+# imagine error as a bowl
+# we want to roll down to the bottom
+# gradient tells us which way is down!
+
+# gradient = slope at current point
+# positive slope = go left (decrease weight)
+# negative slope = go right (increase weight)
+
+print("gradient = direction + steepness")
+print()
+print("steep slope = big gradient = big change needed")
+print("flat slope = small gradient = small change needed")
+print()
+print("we always go OPPOSITE the gradient")
+print("(going downhill on the error curve)")
