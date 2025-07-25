664b2d3636151d55bfbf2b7489a188051fd38a29
Shams Abrar <shams.abrar2611@gmail.com>
2025-07-25 10:00:00 -0400

    Chapter 6: 01_two_layers_idea

---
+++ Chapter 6/01_two_layers_idea.py
@@ -0,0 +1,12 @@
+# two layers - deep learning begins!
+
+# before: input -> output (1 layer)
+# now: input -> hidden -> output (2 layers)
+
+print("single layer:")
+print("  input -> [weights] -> output")
+print()
+print("two layers:")
+print("  input -> [weights_0] -> hidden -> [weights_1] -> output")
+print()
+print("hidden layer learns intermediate representations!")
